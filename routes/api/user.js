14844b8dc4bafe8392d4b654e00ea9bd8cf1e038
aaronl647 <AaronLei@Aarons-MacBook-Pro.local>
2020-03-30 09:35:20 -0400

    got rid of some extra comments

--- routes/api/user.js
+++ routes/api/user.js
@@ -4,15 +4,15 @@ var router = express.Router();
 var userCtrl = require('../../controllers/user');
 
 router.get('/', userCtrl.index) // home page 
-router.post('/', userCtrl.newUser) // Create a new user +
-router.get('/:id', userCtrl.showUser) // Show a user +
-router.put('/edit/:id', userCtrl.editUser) // Update user + 
-router.put('/plan/edit/:id', userCtrl.editSchedule) // Edit ongoing plans +
-router.get('/plan/:id', userCtrl.showPlan) // show ongoing plans +
-router.post('/plan/newplan', userCtrl.newPlan) // Create a new Schedule +
-router.delete('/plan/edit/:id', userCtrl.deleteSchedule) // Deletes schedule +
-router.post('/plan/newplan/activity', userCtrl.newActivity) // Create a new activity +
-router.get('/activity/:id', userCtrl.showActivity) // show ongoing activities +
+router.post('/', userCtrl.newUser) // Create a new user 
+router.get('/:id', userCtrl.showUser) // Show a user 
+router.put('/edit/:id', userCtrl.editUser) // Update user  
+router.put('/plan/edit/:id', userCtrl.editSchedule) // Edit ongoing plans 
+router.get('/plan/:id', userCtrl.showPlan) // show ongoing plans 
+router.post('/plan/newplan', userCtrl.newPlan) // Create a new Schedule 
+router.delete('/plan/edit/:id', userCtrl.deleteSchedule) // Deletes schedule 
+router.post('/plan/newplan/activity', userCtrl.newActivity) // Create a new activity 
+router.get('/activity/:id', userCtrl.showActivity) // show ongoing activities 
 router.delete('/activity/edit/:id', userCtrl.deleteActivity) // Deletes activity
 router.put('/activity/edit/:id', userCtrl.editActivity) // edits ongoing activites
 
